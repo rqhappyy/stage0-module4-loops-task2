b9d428d820d6ccface46e1527a41ddd836d5ed76
rqhappyy <dtereschenko110@gmail.com>
2023-11-14 12:22:34 +0300

    task3

--- src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
+++ src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
@@ -4,7 +4,7 @@ public class PowerOfTwo {
     public void printPower(int power) {
         int i = 0;
         int result = 1;
-        if(power>=32||power<=0) {
+        if(power>=32||power<0) {
             System.out.println("too much power");
         }else {
             while (i <= power) {
